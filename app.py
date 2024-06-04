3b97404f8d6157d37d936a21a559e185a07d0420
msmpunk <msm0102712@gmail.com>
2024-06-04 16:45:06 -0600

    api last

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 from routes.routes import *
 
 if __name__ == "__main__":
     host = '0.0.0.0'  # Listen on all network interfaces
-    port = 8080  # Custom port
+    port = 8102  # Custom port
 
     # Start the Flask application
     app.run(host=host, port=port, debug=True)
--- routes/routes.py
+++ routes/routes.py
@@ -1,58 +1,214 @@
-# Import necessary modules
-from flask import jsonify, request
+# Importar los módulos necesarios
+from flask import jsonify, request, send_file
 from app import app
 import requests
 import json
+import zipfile
+import os
 
-USUARIO_ANONIMO = 'gsierram'
-PASSWORD_ANONIMO = 'FalsoPassword$%'
-url_servidor = 'http://devsys.iingen.unam.mx/geco4/'
+# URL del servidor
+url_servidor = 'http://www.geco.unam.mx/geco3/'
+#url_servidor = 'http://devsys.iingen.unam.mx/geco4/'
 
-# Route to retrieve all items
-@app.route('/api/login', methods=['POST'])
+# Ruta para crear un ítem
+@app.route('/api/login/v1', methods=['POST'])
 def create_item():
     try:
+        data = request.json
+
+        if not data or 'username' not in data or 'password' not in data:
+            return jsonify({'error': 'Falta nombre de usuario o contraseña'}), 400
+        
         url = url_servidor+'proyectos/apidocs/get-token'
-        data = {
-        'username': USUARIO_ANONIMO,
-        'password': PASSWORD_ANONIMO,
+        payload = {
+            'username': data['username'],
+            'password': data['password'],
         }
-        response = requests.post(url, data=data)
+
+        response = requests.post(url, data=payload)
 
         return response.json(), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
     
-# Route to retrieve all items
-@app.route('/api/get_corpus/<int:corpus_id>', methods=['GET'])
-def get_items(corpus_id):
+# Ruta para obtener corpus públicos
+@app.route('/api/get/corpus_publicos/', methods=['GET'])
+def get_corpus():
     try:
         authorization_header = request.headers.get('Authorization')
         
         parts = authorization_header.split()
-        url = url_servidor+f'proyectos/apidocs/corpus/{corpus_id}'
+        
+        url = url_servidor+f'proyectos/apidocs/corpus'
         headers = {'Authorization': 'Token ' + parts[1]}
         response = requests.get(url, headers=headers )
         JSONdocsCorpus = response.json()
-        print(f'Obtenemos el documento {JSONdocsCorpus["data"][0]}')
+        
+        return JSONdocsCorpus, 200
+    except Exception as e:
+        print(e)
+        return jsonify({'error': str(e)}), 500
 
-        for numero in JSONdocsCorpus["data"]:
-            get_items(corpus_id, numero["id"], parts[1])
-            print(numero["id"])
-        return response.json(), 200
+# Ruta para obtener un corpus público por su ID
+@app.route('/api/get_corpus_publico/<int:corpus_id>', methods=['GET'])
+def get_corpus_by_id(corpus_id):
+    try:
+        authorization_header = request.headers.get('Authorization')
+        parts = authorization_header.split()
+        url = url_servidor+f'proyectos/apidocs/corpus/{corpus_id}/tabla'
+        headers = {'Authorization': 'Token ' + parts[1]}
+        response = requests.get(url, headers=headers )
+        JSONdocsCorpus = response.json()
+        return JSONdocsCorpus, 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+# Ruta para obtener texto
+@app.route('/api/get_text/<int:corpus_id>', methods=['POST'])
+def get_text(corpus_id):
+    try:
+        authorization_header = request.headers.get('Authorization')
+        
+        if not authorization_header:
+            return jsonify({'error': 'Falta el encabezado de autorización'}), 400
+        
+        parts = authorization_header.split()
+        if len(parts) != 2 or parts[0].lower() != 'token':
+            return jsonify({'error': 'Formato de encabezado de autorización inválido'}), 400
+                
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({'error': 'Falta el cuerpo de la solicitud o no está en formato JSON'}), 400
+ 
+        for item_doc in data['data']:
+            dowload_documents_txt(corpus_id, item_doc, parts[1])
 
-def get_items(id_corpus, id_doc, token):
-    print(id_corpus, id_doc, token)
-    url = url_servidor + f'proyectos/apidocs/corpus/{id_corpus}/{str(id_doc)}/pos'
+        create_zip_from_folder('texts_files.zip')
+        
+        zip_filename = "texts_files.zip"
+        return send_file(zip_filename, as_attachment=True)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        delete_files()
+
+# Ruta para obtener texto con posiciones
+@app.route('/api/get_text_pos/<int:corpus_id>', methods=['POST'])
+def get_text_pos(corpus_id):
+    try:
+        authorization_header = request.headers.get('Authorization')
+        
+        if not authorization_header:
+            return jsonify({'error': 'Falta el encabezado de autorización'}), 400
+        
+        parts = authorization_header.split()
+        if len(parts) != 2 or parts[0].lower() != 'token':
+            return jsonify({'error': 'Formato de encabezado de autorización inválido'}), 400
+                
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({'error': 'Falta el cuerpo de la solicitud o no está en formato JSON'}), 400
+ 
+        for item_doc in data['data']:
+            dowload_documents_pos(corpus_id, item_doc, parts[1])
+
+        create_zip_from_folder('texts_files_pos.zip')
+        
+        zip_filename = "texts_files_pos.zip"
+        return send_file(zip_filename, as_attachment=True)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        delete_files()
+
+# Ruta para obtener texto adjunto
+@app.route('/api/get_text_adjuntos/<int:corpus_id>', methods=['POST'])
+def get_text_adjuntos(corpus_id):
+    try:
+        authorization_header = request.headers.get('Authorization')
+        
+        if not authorization_header:
+            return jsonify({'error': 'Falta el encabezado de autorización'}), 400
+        
+        parts = authorization_header.split()
+        if len(parts) != 2 or parts[0].lower() != 'token':
+            return jsonify({'error': 'Formato de encabezado de autorización inválido'}), 400
+                
+        data = request.get_json()
+        
+        if not data:
+            return jsonify({'error': 'Falta el cuerpo de la solicitud o no está en formato JSON'}), 400
+ 
+        for item_doc in data['data']:
+            dowload_documents_adjuntos(corpus_id, item_doc, parts[1])
+
+        create_zip_from_folder('texts_files_adjuntos.zip')
+        
+        zip_filename = "texts_files_adjuntos.zip"
+        return send_file(zip_filename, as_attachment=True)
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        delete_files()
+
+# Función para descargar documentos de texto
+def dowload_documents_txt(corpus_id, doc, token):
+    url = url_servidor + f'proyectos/apidocs/corpus/{corpus_id}/{str(doc["id"])}'
     headers = {'Authorization': 'Token ' + token}  
-    response = requests.get(url, headers=headers) 
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:                
-        JSONdocsCorpus = response.json()           
-        file_path = f'{11444444}.txt'  # Ajuste para usar f-string
+        JSONdocsCorpus = response.json()
+        file_name = f'{doc["name"]}'  
+        file_path = os.path.join("output_text", file_name)
         with open(file_path, 'a') as file:
-            # Escribe el JSON en el archivo de texto
             json.dump(JSONdocsCorpus["data"], file, ensure_ascii=False)
-            file.write('\n\n')  # Agrega dos líneas nuevas después del JSON
+
+# Función para descargar documentos de texto con posiciones
+def dowload_documents_pos(corpus_id, doc, token):
+    url = url_servidor + f'proyectos/apidocs/corpus/{corpus_id}/{str(doc["id"])}/pos'
+    headers = {'Authorization': 'Token ' + token}  
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:                
+        JSONdocsCorpus = response.json()
+        file_name = f'{doc["name"]}'  
+        file_path = os.path.join("output_text", file_name)
+        with open(file_path, 'a') as file:
+            json.dump(JSONdocsCorpus["data"], file, ensure_ascii=False)
+
+# Función para descargar documentos adjuntos
+def dowload_documents_adjuntos(corpus_id, doc, token):
+    url = url_servidor + f'proyectos/apidocs/corpus/{corpus_id}/{str(doc["id"])}/adjuntos'
+    headers = {'Authorization': 'Token ' + token}  
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:                
+        JSONdocsCorpus = response.json()
+        file_name = f'{doc["name"]}'  
+        file_path = os.path.join("output_text", file_name)
+        with open(file_path, 'a') as file:
+            json.dump(JSONdocsCorpus["data"], file, ensure_ascii=False)
+
+# Función para crear un archivo ZIP a partir de una carpeta
+def create_zip_from_folder(name):
+    folder_path = "output_text"  
+    try:
+        with zipfile.ZipFile(name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+            for root, _, files in os.walk(folder_path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    arcname = os.path.relpath(file_path, folder_path)
+                    zipf.write(file_path, arcname=arcname)
+    except Exception as e:
+        print(f"Error al crear el archivo ZIP: {str(e)}")
+
+# Función para eliminar archivos temporales
+def delete_files():
+    folder_path = "output_text"
+    txt_files = [file for file in os.listdir(folder_path) if file.endswith(".txt")]
+    for txt_file in txt_files:
+        file_path = os.path.join(folder_path, txt_file)
+        os.remove(file_path)
